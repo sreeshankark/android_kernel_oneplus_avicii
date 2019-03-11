9ec1fe34cb7f506fafad53b8f54a40eb07e1e360
Minchan Kim <minchan@google.com>
2019-03-11 11:39:02 +0900

    Revert "mm: provide speculative fault infrastructure"
    This reverts commit 396b980864960745d22e32e663b384db825e9458.
    
    Bug: 128240262
    Change-Id: I1060d8e43cebca1826a489fa3271279501c8647c
    Signed-off-by: Minchan Kim <minchan@google.com>
    
     Conflicts:
    	include/linux/mm.h
    	mm/memory.c
    
    Change-Id: I46a02b3bb492367bb3da8bb18e65800ce3532f5d

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -374,9 +374,6 @@ extern pgprot_t protection_map[16];
 #define FAULT_FLAG_INTERRUPTIBLE		0x200
 #define FAULT_FLAG_PREFAULT_OLD			0x400
 
-/* Speculative fault, not holding mmap_sem */
-#define FAULT_FLAG_SPECULATIVE  0x200
-
 /*
  * The default fault flags that should be used by most of the
  * arch-specific page fault handlers.
@@ -431,10 +428,6 @@ struct vm_fault {
 	gfp_t gfp_mask;			/* gfp mask to be used for allocations */
 	pgoff_t pgoff;			/* Logical page offset based on vma */
 	unsigned long address;		/* Faulting virtual address */
-#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
-	unsigned int sequence;
-	pmd_t orig_pmd;			/* value of PMD at the time of fault */
-#endif
 	pmd_t *pmd;			/* Pointer to pmd entry matching
 					 * the 'address' */
 	pud_t *pud;			/* Pointer to pud entry matching
@@ -1558,31 +1551,6 @@ int invalidate_inode_page(struct page *page);
 #ifdef CONFIG_MMU
 extern vm_fault_t handle_mm_fault(struct vm_area_struct *vma,
 			unsigned long address, unsigned int flags);
-
-#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
-extern int __handle_speculative_fault(struct mm_struct *mm,
-				      unsigned long address,
-				      unsigned int flags);
-static inline int handle_speculative_fault(struct mm_struct *mm,
-					   unsigned long address,
-					   unsigned int flags)
-{
-	/*
-	 * Try speculative page fault for multithreaded user space task only.
-	 */
-	if (!(flags & FAULT_FLAG_USER) || atomic_read(&mm->mm_users) == 1)
-		return VM_FAULT_RETRY;
-	return __handle_speculative_fault(mm, address, flags);
-}
-#else
-static inline int handle_speculative_fault(struct mm_struct *mm,
-					   unsigned long address,
-					   unsigned int flags)
-{
-	return VM_FAULT_RETRY;
-}
-#endif /* CONFIG_SPECULATIVE_PAGE_FAULT */
-
 extern int fixup_user_fault(struct task_struct *tsk, struct mm_struct *mm,
 			    unsigned long address, unsigned int fault_flags,
 			    bool *unlocked);
