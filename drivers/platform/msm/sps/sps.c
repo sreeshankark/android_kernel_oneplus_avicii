b70cb8bb3b2d500e818590f9930fb211e6e42866
Panchajanya1999 <panchajanya@azure-dev.live>
2020-02-16 15:02:11 +0530

    drivers: platform: msm: sps: Treat SPS_ERR as kernel debug messages
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/platform/msm/sps/sps.c
+++ drivers/platform/msm/sps/sps.c
@@ -2198,7 +2198,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	bam->ipc_log0 = ipc_log_context_create(SPS_IPC_LOGPAGES,
 							bam_name, 0);
 	if (!bam->ipc_log0)
-		SPS_ERR(sps, "%s : unable to create IPC Logging 0 for bam %pa",
+		pr_debug("%s : unable to create IPC Logging 0 for bam %pa",
 					__func__, &bam->props.phys_addr);
 
 	snprintf(bam_name, sizeof(bam_name), "sps_bam_%pa_1",
@@ -2206,7 +2206,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	bam->ipc_log1 = ipc_log_context_create(SPS_IPC_LOGPAGES,
 							bam_name, 0);
 	if (!bam->ipc_log1)
-		SPS_ERR(sps, "%s : unable to create IPC Logging 1 for bam %pa",
+		pr_debug("%s : unable to create IPC Logging 1 for bam %pa",
 					__func__, &bam->props.phys_addr);
 
 	snprintf(bam_name, sizeof(bam_name), "sps_bam_%pa_2",
@@ -2214,7 +2214,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	bam->ipc_log2 = ipc_log_context_create(SPS_IPC_LOGPAGES,
 							bam_name, 0);
 	if (!bam->ipc_log2)
-		SPS_ERR(sps, "%s : unable to create IPC Logging 2 for bam %pa",
+		pr_debug("%s : unable to create IPC Logging 2 for bam %pa",
 					__func__, &bam->props.phys_addr);
 
 	snprintf(bam_name, sizeof(bam_name), "sps_bam_%pa_3",
@@ -2222,7 +2222,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	bam->ipc_log3 = ipc_log_context_create(SPS_IPC_LOGPAGES,
 							bam_name, 0);
 	if (!bam->ipc_log3)
-		SPS_ERR(sps, "%s : unable to create IPC Logging 3 for bam %pa",
+		pr_debug("%s : unable to create IPC Logging 3 for bam %pa",
 					__func__, &bam->props.phys_addr);
 
 	snprintf(bam_name, sizeof(bam_name), "sps_bam_%pa_4",
@@ -2230,7 +2230,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	bam->ipc_log4 = ipc_log_context_create(SPS_IPC_LOGPAGES,
 							bam_name, 0);
 	if (!bam->ipc_log4)
-		SPS_ERR(sps, "%s : unable to create IPC Logging 4 for bam %pa",
+		pr_debug("%s : unable to create IPC Logging 4 for bam %pa",
 					__func__, &bam->props.phys_addr);
 #endif
 
