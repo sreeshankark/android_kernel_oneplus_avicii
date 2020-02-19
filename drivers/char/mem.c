71d3eb116f67e31d5e76296992348a06a1b1df7d
Tyler Nijmeh <tylernij@gmail.com>
2020-02-18 17:31:54 -0800

    drivers: char: mem: Reroute random fops to urandom
    Arter has done a similar commit, where the random fops routed the read
    hook to the urandom_read method. However, this leads to a warning about
    random_read being unused, as well as having the poll hook still linked
    to random_poll. This commit should solve both of those issues, from the
    roots.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -897,12 +897,11 @@ static const struct memdev {
 #endif
 	 [5] = { "zero", 0666, &zero_fops, 0 },
 	 [7] = { "full", 0666, &full_fops, 0 },
-<<<<<<< HEAD
 #ifdef CONFIG_SRANDOM
          [8] = { "random", 0666, &sfops, FMODE_NOWAIT },
          [9] = { "urandom", 0666, &sfops, FMODE_NOWAIT },
 #else
-	 [8] = { "random", 0666, &random_fops, FMODE_NOWAIT },
+	 [8] = { "random", 0666, &urandom_fops, FMODE_NOWAIT },
 	 [9] = { "urandom", 0666, &urandom_fops, FMODE_NOWAIT },
 #endif
 #ifndef CONFIG_HW_RANDOM
