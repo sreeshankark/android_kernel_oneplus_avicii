3f677484cbf0294a98570df26b6dbd5e0bc9dff8
Eric Dumazet <edumazet@google.com>
2020-06-19 12:12:34 -0700

    UPSTREAM: tcp: remove indirect calls for icsk->icsk_af_ops->queue_xmit
    Mitigate RETPOLINE costs in __tcp_transmit_skb()
    by using INDIRECT_CALL_INET() wrapper.
    
    Change-Id: I45842813620f9b190b88485818ee80e0d7cf7215
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1176,6 +1176,9 @@ static void tcp_update_skb_after_send(struct sock *sk, struct sk_buff *skb,
 	list_move_tail(&skb->tcp_tsorted_anchor, &tp->tsorted_sent_queue);
 }
 
+INDIRECT_CALLABLE_DECLARE(int ip_queue_xmit(struct sock *sk, struct sk_buff *skb, struct flowi *fl));
+INDIRECT_CALLABLE_DECLARE(int inet6_csk_xmit(struct sock *sk, struct sk_buff *skb, struct flowi *fl));
+
 /* This routine actually transmits TCP packets queued in by
  * tcp_do_sendmsg().  This is used by both the initial
  * transmission and possible later retransmissions.
@@ -1342,7 +1345,9 @@ static int __tcp_transmit_skb(struct sock *sk, struct sk_buff *skb,
 	oplus_app_monitor_update_app_info(sk, skb, 1, 0);
 	#endif /* OPLUS_FEATURE_APP_MONITOR */
 
-	err = icsk->icsk_af_ops->queue_xmit(sk, skb, &inet->cork.fl);
+	err = INDIRECT_CALL_INET(icsk->icsk_af_ops->queue_xmit,
+				 inet6_csk_xmit, ip_queue_xmit,
+				 sk, skb, &inet->cork.fl);
 
 	//#ifdef OPLUS_FEATURE_NWPOWER
 	oplus_match_tcp_output(sk);
