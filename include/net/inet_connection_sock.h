8020d296023c7152e41cbd56834b5d882d48f232
Lorenz Bauer <lmb@cloudflare.com>
2020-08-21 11:29:43 +0100

    BACKPORT: net: sk_msg: Simplify sk_psock initialization
    Initializing psock->sk_proto and other saved callbacks is only
    done in sk_psock_update_proto, after sk_psock_init has returned.
    The logic for this is difficult to follow, and needlessly complex.
    
    Instead, initialize psock->sk_proto whenever we allocate a new
    psock. Additionally, assert the following invariants:
    
    * The SK has no ULP: ULP does it's own finagling of sk->sk_prot
    * sk_user_data is unused: we need it to store sk_psock
    
    Protect our access to sk_user_data with sk_callback_lock, which
    is what other users like reuseport arrays, etc. do.
    
    The result is that an sk_psock is always fully initialized, and
    that psock->sk_proto is always the "original" struct proto.
    The latter allows us to use psock->sk_proto when initializing
    IPv6 TCP / UDP callbacks for sockmap.
    
    Also fix skmsg.c compilation, and align include/linux/bpf.h
    
    Change-Id: If8d7b6a9e3b7341d0dba5705e0f2044234f39fd3
    Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/20200821102948.21918-2-lmb@cloudflare.com

--- include/net/inet_connection_sock.h
+++ include/net/inet_connection_sock.h
@@ -323,6 +323,7 @@ void inet_csk_update_fastreuse(struct inet_bind_bucket *tb,
 
 struct dst_entry *inet_csk_update_pmtu(struct sock *sk, u32 mtu);
 
+<<<<<<< HEAD
 #define TCP_PINGPONG_THRESH	3
 
 static inline void inet_csk_enter_pingpong_mode(struct sock *sk)
@@ -346,5 +347,9 @@ static inline void inet_csk_inc_pingpong_cnt(struct sock *sk)
 
 	if (icsk->icsk_ack.pingpong < U8_MAX)
 		icsk->icsk_ack.pingpong++;
+
+static inline bool inet_csk_has_ulp(struct sock *sk)
+{
+	return inet_sk(sk)->is_icsk && !!inet_csk(sk)->icsk_ulp_ops;
 }
 #endif /* _INET_CONNECTION_SOCK_H */
