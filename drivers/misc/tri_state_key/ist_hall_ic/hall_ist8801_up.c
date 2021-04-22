6d8714345ab699714d20d0073be809ef5f6174d5
LibXZR <xzr467706992@163.com>
2021-04-22 22:56:17 +0800

    tri_state_key: ist_hall_ic: Fix unbalanced irq usage
    * This silent the following warning
    
    [   75.495532] ------------[ cut here ]------------
    [   75.495558] Unbalanced enable for IRQ 459
    [   75.495589] WARNING: CPU: 0 PID: 798 at kernel/irq/manage.c:623 enable_irq+0x90/0xe4
    [   75.495606] CPU: 0 PID: 798 Comm: kworker/0:5 Tainted: G S      W         4.19.185-Horizon-R12.3 #2
    [   75.495608] Hardware name: Qualcomm Technologies, Inc. kona MTP 19805 20809 (DT)
    [   75.495621] Workqueue: events tri_key_dev_work
    [   75.495626] pstate: 60c00085 (nZCv daIf +PAN +UAO)
    [   75.495629] pc : enable_irq+0x90/0xe4
    [   75.495632] lr : enable_irq+0x90/0xe4
    [   75.495634] sp : ffffff80095d3d00
    [   75.495636] x29: ffffff80095d3d10 x28: 00000000000001c8
    [   75.495640] x27: ffffffa950a3e000 x26: ffffffa950a3e000
    [   75.495642] x25: ffffffa95083d000 x24: ffffffa950a3e000
    [   75.495645] x23: ffffffa950a3e000 x22: ffffffa95083d000
    [   75.495648] x21: 00000011905d907a x20: 00000000000001cb
    [   75.495651] x19: fffffffb96950800 x18: 0000000000000000
    [   75.495653] x17: 0000000000000000 x16: 0000000000000001
    [   75.495656] x15: ffffffa9500984b8 x14: 0720072007200720
    [   75.495659] x13: 0000000000002a28 x12: 0000000000000000
    [   75.495662] x11: 0000000000000000 x10: 0000000000000007
    [   75.495664] x9 : 7ddb4bde6ca9d500 x8 : 7ddb4bde6ca9d500
    [   75.495667] x7 : 0720072007200720 x6 : fffffffabfc40f00
    [   75.495670] x5 : 0000000000000003 x4 : 0000000000000000
    [   75.495672] x3 : 0000000000000000 x2 : 0000000000000007
    [   75.495675] x1 : 0000000000000006 x0 : 000000000000001d
    [   75.495681] Call trace:
    [   75.495684] enable_irq+0x90/0xe4
    [   75.495688] ist8801_enable_irq+0x24/0x58
    [   75.495691] tri_key_dev_work+0x324/0xb70
    [   75.495697] process_one_work+0x1c8/0x494
    [   75.495700] worker_thread+0x314/0x544
    [   75.495705] kthread+0x15c/0x1d8
    [   75.495710] ret_from_fork+0x10/0x18
    [   75.495713] ---[ end trace e4da29cf73909027 ]---
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: I1e3dbe20c838858fd163b52d7b037c2580edeceb

--- drivers/misc/tri_state_key/ist_hall_ic/hall_ist8801_up.c
+++ drivers/misc/tri_state_key/ist_hall_ic/hall_ist8801_up.c
@@ -582,6 +582,8 @@ static int ist8801_set_operation_mode(struct ist8801_data_t *ist8801_data,
 }
 
 
+static bool irq_enabled;
+
 /* functions for interrupt handler */
 static irqreturn_t ist8801_up_irq_handler(int irq, void *dev_id)
 {
@@ -593,6 +595,7 @@ static irqreturn_t ist8801_up_irq_handler(int irq, void *dev_id)
 	}
 
 	disable_irq_nosync(g_ist8801_data->irq);
+	irq_enabled = false;
 	__pm_wakeup_event(&g_ist8801_data->source, 2000);
 	oplus_hall_irq_handler(0);
 
@@ -675,6 +678,7 @@ static int ist8801_set_detection_mode(u8 mode)
 			}
 
 			disable_irq(g_ist8801_data->irq);
+			irq_enabled = false;
 			free_irq(g_ist8801_data->irq, NULL);
 
 			g_ist8801_data->irq_enabled = 0;
@@ -691,10 +695,15 @@ static int ist8801_enable_irq(bool enable)
 		return -EINVAL;
 	}
 
+	if (enable == irq_enabled)
+		return 0;
+
 	if (enable)
 		enable_irq(g_ist8801_data->irq);
+		irq_enabled = true;
 	else
 		disable_irq_nosync(g_ist8801_data->irq);
+		irq_enabled = false;
 	return 0;
 }
 
