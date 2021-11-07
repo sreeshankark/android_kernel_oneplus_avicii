a191c4782969a5d0cb1d477ce8814e45096c591b
Dakkshesh <dakkshesh5@gmail.com>
2021-11-07 15:57:48 +0530

    kprofiles: Introduce 2 new API for changing mode on custom events
    This commit introduces two new APIs for other drivers
    to request a profile mode change. So this new APIs gives us the flexibility to
    change profile on any custom event.
    
    kprofiles_set_mode_rollback() : This function takes 2 parameters as input, 1st is the profile mode that it will switch to
    and other for the amount of time (in milliseconds) to stay at that mode before switching back to the previously active mode.
    
    kprofiles_set_mode() : This function is rather simple compared to the previous function, it only takes 1 input parameter,
    which is the mode to switch the profile to, the mode will not change until this function is manually used again to change the mode.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -10,6 +10,42 @@
 unsigned int mode = 0;
 module_param(mode, uint, 0664);
 
+#include <linux/delay.h>
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
+#include <linux/msm_drm_notify.h>
+#elif defined(CONFIG_AUTO_KPROFILES_FB)
+#include <linux/fb.h>
+#endif
+
+static bool screen_on = true;
+static unsigned int mode = 0;
+static unsigned int set_mode;
+module_param(mode, uint, 0664);
+
+void kprofiles_set_mode_rollback(unsigned int level, unsigned int duration_ms)
+{
+	if (!level || !duration_ms)
+		return;
+	set_mode = mode;
+	mode = level;
+	msleep(duration_ms);
+	mode = set_mode;
+}
+
+void kprofiles_set_mode(unsigned int level)
+{
+	if (!level)
+		return;
+	mode = level;
+}
+
+#if defined(CONFIG_AUTO_KPROFILES_MSM_DRM) || defined(CONFIG_AUTO_KPROFILES_FB)
+static int common_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
+{
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
+	struct msm_drm_notifier *evdata = data;
+	int *blank;
 
 unsigned int active_mode(void) {
 	if (mode == 1) {
