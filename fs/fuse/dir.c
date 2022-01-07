fe1ff616ae141202aa5b860e9b7f0fc969f35c4d
Paul Lawrence <paullawrence@google.com>
2022-01-07 20:36:53 +0000

    UPSTREAM: ANDROID: fuse-bpf: Fix perms on readdir
    Add checks for both fuse accesses and backing fs accesses
    
    Bug: 202785178
    Test: fuse_test passes, also atest ScopedStorageDeviceTest passes
    Change-Id: Ida7d90e14ca36588a8cc19453e0d40b4f6f41aa9
    Signed-off-by: Paul Lawrence <paullawrence@google.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1571,6 +1571,7 @@ static int fuse_permission(struct inode *inode, int mask)
 	struct fuse_conn *fc = get_fuse_conn(inode);
 	bool refreshed = false;
 	int err = 0;
+	struct fuse_inode *fi = get_fuse_inode(inode);
 
 	if (fuse_is_bad(inode))
 		return -EIO;
@@ -1583,7 +1584,6 @@ static int fuse_permission(struct inode *inode, int mask)
 	 */
 	if (fc->default_permissions ||
 	    ((mask & MAY_EXEC) && S_ISREG(inode->i_mode))) {
-		struct fuse_inode *fi = get_fuse_inode(inode);
 		u32 perm_mask = STATX_MODE | STATX_UID | STATX_GID;
 
 		if (perm_mask & READ_ONCE(fi->inval_mask) ||
@@ -1623,6 +1623,8 @@ static int fuse_permission(struct inode *inode, int mask)
 			if (!err && !(inode->i_mode & S_IXUGO))
 				return -EACCES;
 		}
+	} else if (!(mask & MAY_NOT_BLOCK) && fi->backing_inode) {
+		err = fuse_access(inode, mask);
 	}
 	return err;
 }
