0351ddc20cf8843ea8b09a87c18529e9a9d0cce9
Prashant1873 <shuklaprashant847@gmail.com>
2022-04-17 16:39:58 +0530

    kernel/power: faster deepsleep

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -27,7 +27,7 @@
  * Timeout for stopping processes
  */
 #ifndef OPLUS_FEATURE_POWERINFO_STANDBY
-unsigned int __read_mostly freeze_timeout_msecs = 20 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 10 * MSEC_PER_SEC;
 #else
 unsigned int __read_mostly freeze_timeout_msecs = 2 * MSEC_PER_SEC;
 #endif /*OPLUS_FEATURE_POWERINFO_STANDBY*/
