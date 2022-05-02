9af31c64f99e45973923897c07f83f4e7074d50e
dakkshesh07 <dakkshesh5@gmail.com>
2022-05-02 16:24:29 +0530

    drivers: cpu_input_boost: Skip boosting if Profile is set to Battery Mode

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -15,6 +15,7 @@
 #include <linux/version.h>
 #include <drm/drm_panel.h>
 #include <linux/sched.h>
+#include <linux/kprofiles.h>
 
 /* The sched_param struct is located elsewhere in newer kernels */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
@@ -172,7 +173,7 @@ static void update_online_cpu_policy(void)
 
 static void __cpu_input_boost_kick(struct boost_drv *b)
 {
-	if (!test_bit(SCREEN_ON, &b->state))
+	if (!test_bit(SCREEN_ON, &b->state) || active_mode() == 1)
 		return;
 
 	if (!input_boost_duration)
@@ -199,7 +200,7 @@ static void __cpu_input_boost_kick_max(struct boost_drv *b,
 	unsigned long boost_jiffies = msecs_to_jiffies(duration_ms);
 	unsigned long curr_expires, new_expires;
 
-	if (!test_bit(SCREEN_ON, &b->state))
+	if (!test_bit(SCREEN_ON, &b->state) || active_mode() == 1)
 		return;
 
 	do {
