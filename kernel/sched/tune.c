04c7e1c28896e37fbf517f4680c7e144b868b627
Prashant1873 <shuklaprashant847@gmail.com>
2022-05-15 17:18:10 +0530

    kprofiles: Skip schedtune boost and prefer_idle on battery mode

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -6,6 +6,7 @@
 #include <linux/printk.h>
 #include <linux/rcupdate.h>
 #include <linux/slab.h>
+#include <linux/kprofiles.h>
 
 #include <trace/events/sched.h>
 
@@ -607,7 +608,7 @@ int schedtune_task_boost(struct task_struct *p)
 {
 	int task_boost;
 
-	if (unlikely(!schedtune_initialized))
+	if (unlikely(!schedtune_initialized) || unlikely(active_mode() == 1))
 		return 0;
 
 	/* Get task boost value */
@@ -623,7 +624,7 @@ int schedtune_prefer_idle(struct task_struct *p)
 	struct schedtune *st;
 	int prefer_idle;
 
-	if (unlikely(!schedtune_initialized))
+	if (unlikely(!schedtune_initialized) || unlikely(active_mode() == 1))
 		return 0;
 
 	/* Get prefer_idle value */
@@ -656,6 +657,9 @@ static u64
 prefer_idle_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
+	
+	if (unlikely(active_mode() == 1))
+		return 0;
 
 	return st->prefer_idle;
 }
@@ -705,6 +709,9 @@ static s64
 boost_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
+
+	if (unlikely(active_mode() == 1))
+		return 0;
 
 	return st->boost;
 }
