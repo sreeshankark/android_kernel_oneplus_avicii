752bb1724cec94e6f3d6e09f7a2825881044cc2f
Prashant1873 <raavimanikanta6595@gmail.com>
2022-06-12 21:36:42 +0530

    Revert "sysctl: rename /proc/sys/vm/swappiness to rswappiness"
    This reverts commit 32b458565b129ff33c6500addb34ea24252f872a.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2006,7 +2006,7 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &zero,
 	},
 	{
-		.procname	= "rswappiness",
+		.procname	= "swappiness",
 		.data		= &vm_swappiness,
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0444,
