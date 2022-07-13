6b9a533b9c608c5af9887f70b5382c8825ae6c2d
Prashant Shukla <shuklaprashant847@gmail.com>
2022-07-13 18:08:13 +0000

    mm: edit values

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -193,7 +193,7 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 100;
+int vm_swappiness = 80;
 #ifdef CONFIG_DYNAMIC_TUNNING_SWAPPINESS
 int vm_swappiness_threshold1 = 0;
 int vm_swappiness_threshold2 = 0;
