9b8047e55291fe400d8b687d12c0eac87d8d7df8
LibXZR <i@xzr.moe>
2023-05-14 11:14:49 +0800

    treewide: Clean up oplus rpmh powerinfo changes
    We don't need this in a release build.
    
    Change-Id: I53ad4c0ec12f874ad0dd3017933b24cedf9bb4c1
    Signed-off-by: LibXZR <i@xzr.moe>

--- drivers/soc/oplus/owakelock/oplus_wakelock_profiler.c
+++ drivers/soc/oplus/owakelock/oplus_wakelock_profiler.c
@@ -123,10 +123,6 @@ int wakeup_reasons_statics(const char *irq_name, int choose_flag)
 	return true;
 }
 
-#ifdef OPLUS_FEATURE_POWERINFO_RPMH
-extern int rpmh_modem_sleepinfo_buffer_clear(void);
-#endif /*OPLUS_FEATURE_POWERINFO_RPMH*/
-
 void wakeup_reasons_clear(int choose_flag)
 {
 	int i, j;
@@ -147,9 +143,6 @@ void wakeup_reasons_clear(int choose_flag)
 			desc->module_all_count = 0;
 		}
 	}
-	#ifdef OPLUS_FEATURE_POWERINFO_RPMH
-	rpmh_modem_sleepinfo_buffer_clear();
-	#endif /*OPLUS_FEATURE_POWERINFO_RPMH*/
 }
 
 void wakeup_reasons_print(int choose_flag, int datil)
