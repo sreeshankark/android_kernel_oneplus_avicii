680fc8e6606ad057c1afac7fd847da37a3451e61
sreeshankark <sreeshankar0910@gmail.com>
2023-09-28 13:06:39 +0530

    NeverSettle Kernel v1.9.5

--- build.py
+++ build.py
@@ -57,8 +57,8 @@ objdir="${kernel_dir}/out"
 kf=$HOME/kf
 builddir="${kernel_dir}/build"
 ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image.gz-dtb
-ns_version=v1.9.402
-kernel_version=4.19.294
+ns_version=v1.9.5
+kernel_version=4.19.295
 ksu_apk_name=KernelSU_v0.6.8_11238-release.apk
 ksu_apk=https://github.com/tiann/KernelSU/releases/download/v0.6.8/KernelSU_v0.6.8_11238-release.apk
 kernel_name="NeverSettle-Kernel-$ns_version-avicii"
@@ -83,7 +83,7 @@ git submodule init && git submodule update
 
 #start off by sending a trigger msg
 tg_post_sticker
-tg_post_msg "<b>NeverSettle Kernel Build Triggered ⌛</b>%0A<b>==============================</b>%0A<b>Kernel : </b><code>$kernel_name</code>%0A<b>Machine : </b><code>$os</code>%0A<b>Cores : </b><code>$cores</code>%0A<b>Time : </b><code>$time</code>"
+tg_post_msg "<b>NeverSettle Kernel Build Triggered ⌛</b>%0A<b>==============================</b>%0A%0A<b>Kernel : </b><code>$kernel_name</code>%0A<b>Machine : </b><code>$os</code>%0A<b>Cores : </b><code>$cores</code>%0A<b>Time : </b><code>$time</code>"
 
 # Colors
 NC='\\033[0m'
@@ -128,10 +128,10 @@ completion() {
     DIFF=$(($END - $START))
     BUILDTIME=$(echo $((${END} - ${START})) | awk '{print int ($1/3600)" Hours:"int(($1/60)%60)"Minutes:"int($1%60)" Seconds"}')
     tg_post_build "$HOME/$zip_name" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
-    tg_post_msg "<b>Changelog ($(date +%d-%m-%Y))</b>%0A<code>$CHANGELOG</code>"
+    tg_post_msg "<b>Changelog ($(date +%d-%m-%Y))</b>%0A%0A<code>$CHANGELOG</code>"
     tg_post_build "$HOME/${ksu_apk_name}" "KernelSU Manager for this build"
     tg_post_msg "<code>Compiled successfully✅</code>"
-    tg_post_msg "<b>Support the developer❤️</b>%0A%0A<b>UPI:</b> <code>sreeshankark@axl</code>%0A%0A<b>Paypal:</b> PayPal.me/SreeshankarK"
+    tg_post_msg "<b>Support the developer❤️</b>%0A%0A<b>UPI:</b> <code>sreeshankark@axl</code>%0A<b>Paypal:</b> PayPal.me/SreeshankarK"
     curl --upload-file $HOME/$zip_name https://free.keep.sh
     echo
     echo -e ${LGR} "############################################"
