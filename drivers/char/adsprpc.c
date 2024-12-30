cfba9fa6a965a6f484861d0cf45b6a90c0ef91bb
Abhinav Parihar <quic_parihar@quicinc.com>
2024-12-30 14:48:16 +0530

    msm: adsprpc: Avoid double free on map
    Decrement and check the ref count of map
    inside the lock. Otherwise, two threads may
    free the same map.
    
    Change-Id: I081b937bfd3e8da3e2480f062cad6966662994b5
    Acked-by: Sharad Kumar <sharku@qti.qualcomm.com>
    Signed-off-by: Abhinav Parihar <quic_parihar@quicinc.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -913,9 +913,11 @@ static void fastrpc_mmap_free(struct fastrpc_mmap *map, uint32_t flags)
 		map->refs--;
 		if (!map->refs && !map->ctx_refs)
 			hlist_del_init(&map->hn);
-		spin_unlock(&me->hlock);
-		if (map->refs > 0)
+		if (map->refs > 0) {
+			spin_unlock(&me->hlock);
 			return;
+		}
+		spin_unlock(&me->hlock);
 	} else {
 		map->refs--;
 		if (!map->refs && !map->ctx_refs)
