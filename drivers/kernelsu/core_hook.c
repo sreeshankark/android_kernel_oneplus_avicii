4e6fb687cb1a850390b58e2e73f3df2d9785a7f1
sreeshankark <sreeshankar0910@gmail.com>
2025-09-22 03:59:07 +0000

    drivers: Update KernelSU-Next to v1.1.1 with SUSFS v1.5.9

--- drivers/kernelsu/core_hook.c
+++ drivers/kernelsu/core_hook.c
@@ -8,7 +8,6 @@
 #include <linux/kernel.h>
 #include <linux/kprobes.h>
 #ifdef CONFIG_KSU_LSM_SECURITY_HOOKS
-#include <linux/binfmts.h>
 #include <linux/lsm_hooks.h>
 #endif
 #include <linux/mm.h>
@@ -1383,49 +1382,7 @@ int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
 }
 #endif
 
-#ifdef CONFIG_COMPAT
-bool ksu_is_compat __read_mostly = false;
-#endif
-
 #ifdef CONFIG_KSU_LSM_SECURITY_HOOKS
-
-int ksu_bprm_check(struct linux_binprm *bprm)
-{
-	char *filename = (char *)bprm->filename;
-
-#ifndef CONFIG_KSU_KPROBES_HOOK
-	if (likely(!ksu_execveat_hook))
-		return 0;
-#endif
-
-/*
- * 32-on-64 compat detection 
- *
- * notes:
- * bprm->buf provides the binary itself !!
- * https://unix.stackexchange.com/questions/106234/determine-if-a-specific-process-is-32-or-64-bit
- * buf[0] == 0x7f && buf[1] == 'E' &&  buf[2] == 'L' && buf[3] == 'F' 
- * so as that said, we check ELF header, then we check 5th byte, 0x01 = 32-bit, 0x02 = 64 bit
- * we only check first execution of /data/adb/ksud and while ksu_execveat_hook is open!
- * 
- */
-#ifdef CONFIG_COMPAT
-	static bool compat_check_done __read_mostly = false;
-	if ( unlikely(!compat_check_done) && unlikely(!strcmp(filename, "/data/adb/ksud"))
-		&& !memcmp(bprm->buf, "\x7f\x45\x4c\x46", 4) ) {
-		if (bprm->buf[4] == 0x01 )
-			ksu_is_compat = true;
-		pr_info("%s: %s ELF magic found! ksu_is_compat: %d \n", __func__, filename, ksu_is_compat);
-		compat_check_done = true;
-	}
-#endif
-
-	ksu_handle_pre_ksud(filename);
-
-	return 0;
-
-}
-
 static int ksu_task_prctl(int option, unsigned long arg2, unsigned long arg3,
 			  unsigned long arg4, unsigned long arg5)
 {
@@ -1447,7 +1404,6 @@ static int ksu_task_fix_setuid(struct cred *new, const struct cred *old,
 
 #ifndef MODULE
 static struct security_hook_list ksu_hooks[] = {
-	LSM_HOOK_INIT(bprm_check_security, ksu_bprm_check),
 	LSM_HOOK_INIT(task_prctl, ksu_task_prctl),
 	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
 	LSM_HOOK_INIT(task_fix_setuid, ksu_task_fix_setuid),
