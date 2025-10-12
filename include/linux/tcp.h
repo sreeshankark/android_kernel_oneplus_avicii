0235183dbbfb9a84b97b6bfef03b3549efbe9e1c
sreeshankark <sreeshankar0910@gmail.com>
2025-10-12 05:05:18 +0000

    include: linux: Remove tcp_clock_cache redefinition

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -256,8 +256,6 @@ struct tcp_sock {
 	u64	tcp_wstamp_ns;	/* departure time for next sent data packet */
 	u64	tcp_clock_cache; /* cache last tcp_clock_ns() (see tcp_mstamp_refresh()) */
 
-	u64	tcp_clock_cache; /* cache last tcp_clock_ns() (see tcp_mstamp_refresh()) */
-
 /* RTT measurement */
 	u64	tcp_mstamp;	/* most recent packet received/sent */
 	u32	srtt_us;	/* smoothed round trip time << 3 in usecs */
