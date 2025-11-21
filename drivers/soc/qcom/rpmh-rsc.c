4c441112b12882c0c3a8898487713d05eba26561
Thomas Turner <thomas_turner@talktalk.net>
2025-11-21 22:56:56 +0000

    fixup! drivers: qcom: rpmh-rsc: clear active mode configuration for waketcs
    Apply upstream commit c15a40c95a6108831b5ac3d4b42e35c5cefa69fd
    correctly.
    
    Change-Id: I12461c5e980d275c0bc29f724edcdb79e5bd138d

--- drivers/soc/qcom/rpmh-rsc.c
+++ drivers/soc/qcom/rpmh-rsc.c
@@ -218,7 +218,7 @@ static const struct tcs_request *get_req_from_tcs(struct rsc_drv *drv,
 	return NULL;
 }
 
-static void __tcs_trigger(struct rsc_drv *drv, int tcs_id, bool trigger)
+static void __tcs_set_trigger(struct rsc_drv *drv, int tcs_id, bool trigger)
 {
 	u32 enable;
 
@@ -243,7 +243,7 @@ static void __tcs_trigger(struct rsc_drv *drv, int tcs_id, bool trigger)
 	}
 }
 
-static inline void enable_tcs_irq(struct rsc_drv *drv, int tcs_id, bool enable)
+static void enable_tcs_irq(struct rsc_drv *drv, int tcs_id, bool enable)
 {
 	u32 data;
 
@@ -294,10 +294,13 @@ static irqreturn_t tcs_tx_done(int irq, void *p)
 		ipc_log_string(drv->ipc_log_ctx,
 			       "IRQ response: m=%d err=%d", i, err);
 
-		/* Clear AMC trigger & enable modes and
+		/*
+		 * If wake tcs was re-purposed for sending active
+		 * votes, clear AMC trigger & enable modes and
 		 * disable interrupt for this TCS
 		 */
-		__tcs_set_trigger(drv, i, false);
+		if (!drv->tcs[ACTIVE_TCS].num_tcs)
+			__tcs_set_trigger(drv, i, false);
 skip:
 		/* Reclaim the TCS */
 		write_tcs_reg(drv, RSC_DRV_CMD_ENABLE, i, 0);
@@ -429,7 +432,7 @@ static int tcs_write(struct rsc_drv *drv, const struct tcs_request *msg)
 		enable_tcs_irq(drv, tcs_id, true);
 
 	__tcs_buffer_write(drv, tcs_id, 0, msg);
-	__tcs_trigger(drv, tcs_id, true);
+	__tcs_set_trigger(drv, tcs_id, true);
 
 done_write:
 	spin_unlock(&drv->lock);
