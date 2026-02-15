f84594a3824dd286bef51245ed0b807c8d96f9d4
sreeshankark <sreeshankar0910@gmail.com>
2026-02-15 08:00:45 +0000

    Revert "treewide: Remove oplus iomonitor"
    This reverts commit d890b58730d51e499bf0e47f290000f363286ddd.

--- block/blk-core.c
+++ block/blk-core.c
@@ -46,6 +46,10 @@
 #include "blk-mq-sched.h"
 #include "blk-rq-qos.h"
 
+#if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
+#include <linux/iomonitor/iomonitor.h>
+#endif /*OPLUS_FEATURE_IOMONITOR*/
+
 #ifdef CONFIG_DEBUG_FS
 struct dentry *blk_debugfs_root;
 #endif
@@ -1478,6 +1482,9 @@ out:
 	 */
 	if (ioc_batching(q, ioc))
 		ioc->nr_batch_requests--;
+#if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
+	iomonitor_init_reqstats(rq);
+#endif /*OPLUS_FEATURE_IOMONITOR*/
 	trace_block_getrq(q, bio, op);
 	return rq;
 
@@ -2509,11 +2516,17 @@ blk_qc_t submit_bio(struct bio *bio)
 
 		if (op_is_write(bio_op(bio))) {
 			count_vm_events(PGPGOUT, count);
+#if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
+			iomonitor_update_vm_stats(PGPGOUT, count);
+#endif /*OPLUS_FEATURE_IOMONITOR*/
 		} else {
 			if (bio_flagged(bio, BIO_WORKINGSET))
 				workingset_read = true;
 			task_io_account_read(bio->bi_iter.bi_size);
 			count_vm_events(PGPGIN, count);
+#if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
+			iomonitor_update_vm_stats(PGPGIN, count);
+#endif /*OPLUS_FEATURE_IOMONITOR*/
 		}
 
 		if (unlikely(block_dump)) {
@@ -2866,6 +2879,9 @@ struct request *blk_peek_request(struct request_queue *q)
 			 * not be passed by new incoming requests
 			 */
 			rq->rq_flags |= RQF_STARTED;
+#if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
+			rq->req_td = ktime_get();
+#endif /*OPLUS_FEATURE_IOMONITOR*/
 
 			trace_block_rq_issue(q, rq);
 		}
@@ -2926,6 +2942,9 @@ struct request *blk_peek_request(struct request_queue *q)
 			break;
 		}
 	}
+#if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
+	iomonitor_record_io_history(rq);
+#endif /*OPLUS_FEATURE_IOMONITOR*/
 	return rq;
 }
 EXPORT_SYMBOL(blk_peek_request);
@@ -3066,6 +3085,9 @@ bool blk_update_request(struct request *req, blk_status_t error,
 	int total_bytes;
 
 	trace_block_rq_complete(req, blk_status_to_errno(error), nr_bytes);
+#if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
+	iomonitor_record_reqstats(req, nr_bytes);
+#endif /*OPLUS_FEATURE_IOMONITOR*/
 
 	if (!req->bio)
 		return false;
