425922b7c2893cb7a19aaeeaced2d68a27499b7d
sreeshankark <sreeshankar0910@gmail.com>
2026-02-15 08:06:30 +0000

    Revert "Makefile/Kconfig: Remove oplus aging test"
    This reverts commit 36b2309a37a5866f88f68790494304d9ac5ea402.

--- kernel/panic.c
+++ kernel/panic.c
@@ -32,6 +32,9 @@
 #include <linux/sysfs.h>
 #include <asm/sections.h>
 #include <soc/qcom/minidump.h>
+#ifdef OPLUS_FEATURE_AGINGTEST
+#include <linux/soc/qcom/smem.h>
+#endif /*OPLUS_FEATURE_AGINGTEST*/
 
 #define PANIC_TIMER_STEP 100
 #define PANIC_BLINK_SPD 18
@@ -258,6 +261,10 @@ void panic(const char *fmt, ...)
 	if (vendor_panic_cb)
 		vendor_panic_cb(0);
 	pr_emerg("Kernel panic - not syncing: %s\n", buf);
+#ifdef OPLUS_FEATURE_AGINGTEST
+	function_name = parse_function_builtin_return_address((unsigned long)__builtin_return_address(0));
+	save_dump_reason_to_smem(buf, function_name);
+#endif /*OPLUS_FEATURE_AGINGTEST*/
 
 #ifdef CONFIG_DEBUG_BUGVERBOSE
 	/*
