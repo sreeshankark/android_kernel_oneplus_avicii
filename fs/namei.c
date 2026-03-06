f05ece9d2a819c50a7af7729fb74e7066692ed11
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-03-06 05:10:08 +0000

    fs: susfs: change inode->i_mapping->flags to inode->i_state for storing AS_FLAGS_ bit Some of the susfs features are working temporarily and did not persist after the command was executed. Such as when opening the app then close it and wait for few seconds to few mins then the detection came back.
    This might be due to storing SUSFS 'AS_FLAGS_' to inode->i_mapping->flags may not be working on non-gki.
    On SUSFS v1.5.5 - v1.5.9, inode->i_state is used to store 'AS_FLAGS_' on non-gki kernels.
    
    So this commit reverts it to use inode->i_state to store 'AS_FLAGS_' on most of the susfs features.

--- fs/namei.c
+++ fs/namei.c
@@ -1024,7 +1024,7 @@ static inline int may_follow_link(struct nameidata *nd)
 	kuid_t puid;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (nd->inode && unlikely(nd->inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+	if (nd->inode && unlikely(nd->inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 		return -ENOENT;
 	}
 #endif
@@ -1107,7 +1107,7 @@ static int may_linkat(struct path *link)
 	struct inode *inode = link->dentry->d_inode;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (link->dentry->d_inode && unlikely(link->dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+	if (link->dentry->d_inode && unlikely(link->dentry->d_inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 		return -ENOENT;
 	}
 #endif
@@ -1154,7 +1154,7 @@ static int may_create_in_sticky(umode_t dir_mode, kuid_t dir_uid,
 				struct inode * const inode)
 {
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (unlikely(inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+	if (unlikely(inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 		return -ENOENT;
 	}
 #endif
@@ -2662,7 +2662,7 @@ int filename_lookup(int dfd, struct filename *name, unsigned flags,
 		audit_inode(name, path->dentry, flags & LOOKUP_PARENT);
 	restore_nameidata();
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (!retval && path->dentry->d_inode && unlikely(path->dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+	if (!retval && path->dentry->d_inode && unlikely(path->dentry->d_inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 		putname(name);
 		return -ENOENT;
 	}
@@ -3149,7 +3149,7 @@ static int may_delete(struct vfsmount *mnt, struct inode *dir, struct dentry *vi
 	if (IS_APPEND(dir))
 		return -EPERM;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (unlikely(inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+	if (unlikely(inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 		return -ENOENT;
 	}
 #endif
@@ -3187,7 +3187,7 @@ static inline int may_create(struct vfsmount *mnt, struct inode *dir, struct den
 	struct user_namespace *s_user_ns;
 	audit_inode_child(dir, child, AUDIT_TYPE_CHILD_CREATE);
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (child->d_inode && unlikely(child->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+	if (child->d_inode && unlikely(child->d_inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 		error = inode_permission(dir, MAY_WRITE | MAY_EXEC);
 		if (error) {
 			return error;
@@ -3325,7 +3325,7 @@ static int may_open(const struct path *path, int acc_mode, int flag)
 		return -ENOENT;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (unlikely(inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+	if (unlikely(inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 		return -ENOENT;
 	}
 #endif
@@ -3404,7 +3404,7 @@ static int may_o_create(const struct path *dir, struct dentry *dentry, umode_t m
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 	int error;
 
-	if (dentry->d_inode && unlikely(dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+	if (dentry->d_inode && unlikely(dentry->d_inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 		error = inode_permission(dir->dentry->d_inode, MAY_WRITE | MAY_EXEC);
 		if (error) {
 			return error;
@@ -3594,7 +3594,7 @@ skip_orig_flow2:
 	if (dentry->d_inode) {
 		/* Cached positive dentry: will open in f_op->open */
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (unlikely(dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+		if (unlikely(dentry->d_inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 			dput(dentry);
 			return -ENOENT;
 		}
@@ -3643,7 +3643,7 @@ skip_orig_flow2:
 		if (unlikely(error == -ENOENT) && create_error)
 			error = create_error;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (!IS_ERR(dentry) && dentry->d_inode && unlikely(dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+		if (!IS_ERR(dentry) && dentry->d_inode && unlikely(dentry->d_inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 			if (create_error) {
 				dput(dentry);
 				return create_error;
@@ -3668,7 +3668,7 @@ no_open:
 			dput(dentry);
 			dentry = res;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-			if (dentry->d_inode && unlikely(dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
+			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & BIT_SUS_PATH) && likely(susfs_is_current_proc_umounted())) {
 				dput(dentry);
 				return -ENOENT;
 			}
@@ -4035,7 +4035,7 @@ struct file *do_filp_open(int dfd, struct filename *pathname,
 	if (unlikely(filp == ERR_PTR(-ESTALE)))
 		filp = path_openat(&nd, op, flags | LOOKUP_REVAL);
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	if (!IS_ERR(filp) && unlikely(filp->f_inode->i_mapping->flags & BIT_OPEN_REDIRECT) && current_uid().val < 11000) {
+	if (!IS_ERR(filp) && unlikely(filp->f_inode->i_state & BIT_OPEN_REDIRECT) && current_uid().val < 11000) {
 		fake_pathname = susfs_get_redirected_path(filp->f_inode->i_ino);
 		if (!IS_ERR(fake_pathname)) {
 			restore_nameidata();
